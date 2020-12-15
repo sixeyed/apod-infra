10dc0b94f93b874a7ae604c61b8d49d1e8f23a91
Elton Stoneman <elton@sixeyed.com>
2020-12-15 12:05:34 +0000

    Create logic & logging

--- scripts/aks-create.sh
+++ scripts/aks-create.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-az group create --name $RG_NAME --location eastus
+# create RG if needed:
+az group show --name $RG_NAME > /dev/null
+if [ $? != 0 ]; then
+	echo "Creating new resource group: $RG_NAME"
+	az group create --name $RG_NAME --location eastus > /dev/null
+else
+	echo "Using existing resource group: $$RG_NAME"
+fi
 
-az aks create -g $RG_NAME -n $AKS_NAME \
-  --node-count 2 --kubernetes-version 1.18.10 \
-  --service-principal $AKS_SP_APP_ID \
-  --client-secret $AKS_SP_APP_PASSWORD \
-  --generate-ssh-keys
+# create cluster if not exists:
+az aks show --resource-group $RG_NAME --name $AKS_NAME > /dev/null
+if [ $? != 0 ]; then
+    echo "Creating AKS cluster: $AKS_NAME"
+    az aks create -g $RG_NAME -n $AKS_NAME \
+    --node-count 2 --kubernetes-version 1.18.10 \
+    --service-principal $AKS_SP_APP_ID \
+    --client-secret $AKS_SP_APP_PASSWORD \
+    --generate-ssh-keys
+else
+	echo "Cluster: $AKS_NAME already exists, no changes made - resource group: $RG_NAME"
+fi
--- scripts/argocd-deploy.sh
+++ scripts/argocd-deploy.sh
@@ -2,13 +2,16 @@
 
 az aks get-credentials -g $RG_NAME -n $AKS_NAME
 
+echo "Deploying Argo CD version: $ARGOCD_VERSION"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGOCD_VERSION/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
+echo "Downloading Argo CD CLI version: $ARGOCD_VERSION"
 curl -sSL --create-dirs -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$ARGOCD_VERSION/argocd-linux-amd64
 chmod +x /usr/local/bin/argocd
 
+echo "Configuring Argo CD with cluster: $AKS_NAME"
 PWD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')
 argocd login localhost --insecure --username admin --password $PWD
 argocd cluster add $AKS_NAME
