b5746df9b47a3537cb99ef124c60a90031e980a3
Elton Stoneman <elton@sixeyed.com>
2020-12-15 10:35:21 +0000

    RG name & SSH keygen

--- scripts/aks-create.sh
+++ scripts/aks-create.sh
@@ -2,4 +2,6 @@
 
 az group create --name $RG_NAME --location eastus
 
-az aks create -g $RG_NAME -n $AKS_NAME --node-count 2 --kubernetes-version 1.18.10
+az aks create -g $RG_NAME -n $AKS_NAME \
+  --node-count 2 --kubernetes-version 1.18.10 \
+  --generate-ssh-keys
