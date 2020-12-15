7bab041eb1863858b2b32e926b609c38b3f8fb90
Elton Stoneman <elton@sixeyed.com>
2020-12-15 15:00:10 +0000

    Use Kustomize

--- scripts/argocd-create-apod-app.sh
+++ scripts/argocd-create-apod-app.sh
@@ -4,7 +4,7 @@ kubectl create ns apod
 
 argocd app create apod \
  --repo https://github.com/sixeyed/apod-app.git \
- --path kubernetes \
+ --path kustomize/base \
  --dest-server https://kubernetes.default.svc \
  --dest-namespace apod \
  --sync-policy automated
